272408cb5cd84d5c85ff996305bd1a077fb297c2
klikknpro <klikknpro@gmail.com>
2024-05-17 12:16:01 +0200

    feat: revert

--- src/Player.ts
+++ src/Player.ts
@@ -1,68 +1,64 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    const ourCards = gameState.players[gameState.in_action].hole_cards;
-    const communityCards = gameState.community_cards;
-    const allCards = [...ourCards, ...communityCards];
+    const ourCards = gameState.players[gameState.in_action].hole_cards
+    const communityCards = gameState.community_cards
+    const allCards = [...ourCards, ...communityCards]
 
-    let isHighCards = false;
-    const ourHighCards = [];
+    let isHighCards = false
+    const ourHighCards = []
     ourCards.forEach((card: any) => {
-      if (["A", "K", "Q", "J", "10"].includes(card.rank)) {
-        ourHighCards.push(card);
-        isHighCards = true;
+      if (['A', 'K', 'Q', 'J', '10'].includes(card.rank)) {
+        ourHighCards.push(card)
+        isHighCards = true
       }
-    });
+    })
 
-    let hasPair = false;
-    const cardRanks = allCards.map((card: any) => card.rank);
-    const uniqueCardRanks = new Set(cardRanks);
-    hasPair = uniqueCardRanks.size !== cardRanks.length;
+    let hasPair = false
+    const cardRanks = allCards.map((card: any) => card.rank)
+    const uniqueCardRanks = new Set(cardRanks)
+    hasPair = uniqueCardRanks.size !== cardRanks.length
 
     // const checkForHighPair = () => {
     // }
 
-    const betToCall =
-      gameState.current_buy_in - gameState.players[gameState.in_action].bet;
-    const betToRaise =
-      gameState.current_buy_in -
-      gameState.players[gameState.in_action].bet +
-      gameState.minimum_raise;
-    const betAllIn = gameState.players[gameState.in_action].stack;
+    const betToCall = gameState.current_buy_in - gameState.players[gameState.in_action].bet
+    const betToRaise = gameState.current_buy_in - gameState.players[gameState.in_action].bet + gameState.minimum_raise
+    const betAllIn = gameState.players[gameState.in_action].stack
 
     if (hasPair && isHighCards) {
-      betCallback(betAllIn);
-      return;
+      betCallback(betAllIn)
+      return
     }
 
     if (communityCards.length === 0) {
       // Before flop
       if (hasPair || isHighCards) {
-        betCallback(betToCall);
+        betCallback(betToCall)
       } else {
-        betCallback(0);
+        betCallback(0)
       }
     } else if (communityCards.length === 3) {
       // Flop
       if (hasPair) {
-        betCallback(betToRaise);
+        betCallback(betToRaise)
       } else if (isHighCards) {
-        betCallback(betToCall);
+        betCallback(betToCall)
       } else {
-        betCallback(0);
+        betCallback(0)
       }
     } else if (communityCards.length === 4) {
       // Turn
       if (hasPair || isHighCards) {
-        betCallback(betToRaise);
+        betCallback(betToRaise)
       } else {
-        betCallback(0);
+        betCallback(0)
       }
     } else if (communityCards.length === 5) {
       // River
       if (hasPair || isHighCards) {
-        betCallback(betToRaise);
+        betCallback(betToRaise)
       } else {
-        betCallback(0);
+        betCallback(0)
       }
     }
   }
@@ -72,4 +68,4 @@ export class Player {
   }
 }
 
-export default Player;
+export default Player
