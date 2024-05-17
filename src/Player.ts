9f9223fb8dd9d70ac272ecf49075f710b949e28f
klikknpro <klikknpro@gmail.com>
2024-05-17 10:36:20 +0200

    feat: modify bet logic with pairs and high cards

--- src/Player.ts
+++ src/Player.ts
@@ -1,17 +1,33 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
     const ourCards = gameState.players[gameState.in_action].hole_cards
+    const communityCards = gameState.community_cards
+
     let isHighCards = false
     ourCards.forEach((card: any) => {
-      if (card.rank === 'A' || card.rank === 'K' || card.rank === 'Q' || card.rank === 'J' || card.rank === '10') {
+      if (['A', 'K', 'Q', 'J', '10'].includes(card.rank)) {
         isHighCards = true
       }
     })
 
+    let hasPair = false
+    const ourCardRanks = ourCards.map((card: any) => card.rank)
+    const uniqueCardRanks = new Set(ourCardRanks)
+    hasPair = uniqueCardRanks.size !== ourCardRanks.length
+
     const betToCall = gameState.current_buy_in - gameState.players[gameState.in_action].bet
     const betToRaise = gameState.current_buy_in - gameState.players[gameState.in_action].bet + gameState.minimum_raise
 
-    betCallback(isHighCards ? betToRaise : betToCall)
+    if (hasPair) {
+      betCallback(betToRaise)
+      return
+    } else if (isHighCards) {
+      betCallback(betToCall)
+      return
+    } else {
+      betCallback(0)
+      return
+    }
   }
 
   public showdown(gameState: any): void {
