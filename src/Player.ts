075f8c87a20e1cebb1820ef59cf7ef01a76abf80
klikknpro <klikknpro@gmail.com>
2024-05-17 10:06:43 +0200

    feat: apply minimum bet to call

--- src/Player.ts
+++ src/Player.ts
@@ -1,11 +1,13 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(0);
+    // console.log('gameState', gameState)
+    const betToCall = gameState.current_buy_in - gameState.players[gameState.in_action].bet
+    betCallback(betToCall)
   }
 
   public showdown(gameState: any): void {
-
+    // called at the end of each round
   }
-};
+}
 
-export default Player;
+export default Player
