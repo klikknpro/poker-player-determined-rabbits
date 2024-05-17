1399f6560164b15664660fc70ee5749627bcbc33
klikknpro <klikknpro@gmail.com>
2024-05-17 10:19:48 +0200

    feat: logic to raise or call

--- src/Player.ts
+++ src/Player.ts
@@ -1,8 +1,17 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    // console.log('gameState', gameState)
+    const ourCards = gameState.players[gameState.in_action].hole_cards
+    let isHighCards = false
+    ourCards.forEach((card: any) => {
+      if (card.rank === 'A' || card.rank === 'K' || card.rank === 'Q' || card.rank === 'J' || card.rank === '10') {
+        isHighCards = true
+      }
+    })
+
     const betToCall = gameState.current_buy_in - gameState.players[gameState.in_action].bet
-    betCallback(betToCall)
+    const betToRaise = gameState.current_buy_in - gameState.players[gameState.in_action].bet + gameState.minimum_raise
+
+    betCallback(isHighCards ? betToRaise : betToCall)
   }
 
   public showdown(gameState: any): void {
